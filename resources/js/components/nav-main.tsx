918d1fcf99c2dbbd2f6704c12144a6f5ec4c852c
Muhammed ElFeqy <thefeqy@gmail.com>
2025-02-13 01:53:28 +0200

    Use kebab case for react components

---
+++ resources/js/components/nav-main.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import {ChevronRight, type LucideIcon} from "lucide-react"
+
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarMenuSubButton,
+    SidebarMenuSubItem,
+} from "@/components/ui/sidebar"
+import {Link, router} from "@inertiajs/react";
+
+export function NavMain({
+                            items,
+                        }: {
+    items: {
+        title: string
+        url: string
+        route?: string
+        icon?: LucideIcon
+        isActive?: boolean
+        items?: {
+            title: string
+            url: string
+        }[]
+    }[]
+}) {
+    return (
+        <SidebarGroup>
+            <SidebarGroupLabel>Platform</SidebarGroupLabel>
+            <SidebarMenu>
+                {items.map((item) =>
+                    item.items ? (
+                        <Collapsible
+                            key={item.title}
+                            asChild
+                            defaultOpen={item.isActive}
+                            className="group/collapsible"
+                        >
+                            <SidebarMenuItem>
+                                <CollapsibleTrigger asChild
+                                                    onClick={() => item.url !== '#' && router.push({url: item.url})}>
+                                    <SidebarMenuButton tooltip={item.title} isActive={item.isActive}>
+                                        {item.icon && <item.icon/>}
+                                        <span>{item.title}</span>
+                                        <ChevronRight
+                                            className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90"/>
+                                    </SidebarMenuButton>
+                                </CollapsibleTrigger>
+                                <CollapsibleContent>
+                                    <SidebarMenuSub>
+                                        {item.items.map((subItem) => (
+                                            <SidebarMenuSubItem key={subItem.title}>
+                                                <Link href={subItem.url}>
+                                                    <SidebarMenuSubButton asChild>
+                                                        <a href={subItem.url}>
+                                                            <span>{subItem.title}</span>
+                                                        </a>
+                                                    </SidebarMenuSubButton>
+                                                </Link>
+                                            </SidebarMenuSubItem>
+                                        ))}
+                                    </SidebarMenuSub>
+                                </CollapsibleContent>
+                            </SidebarMenuItem>
+                        </Collapsible>
+                    ) : (
+                        <SidebarMenuItem key={item.url}>
+                            <Link href={item.url}>
+                                <SidebarMenuButton tooltip={item.title} isActive={route().current(item.route as string) ?? false}>
+                                    {item.icon && <item.icon/>}
+                                    <span>{item.title}</span>
+                                </SidebarMenuButton>
+                            </Link>
+                        </SidebarMenuItem>
+                    )
+                )}
+
+            </SidebarMenu>
+        </SidebarGroup>
+    )
+}
