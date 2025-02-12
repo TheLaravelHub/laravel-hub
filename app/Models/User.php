663ad5f1c3b24f8170ce3f30f08f36ff506a9a6a
Muhammed ElFeqy <thefeqy@gmail.com>
2025-02-13 01:56:15 +0200

    Add status & soft deletes to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,15 +4,17 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Thefeqy\ModelStatus\Traits\HasActiveScope;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
 
-    use Notifiable;
+    use Notifiable, HasActiveScope, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
