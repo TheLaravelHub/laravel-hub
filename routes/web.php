2eeec8f06ce647b2754f7b95f4eb0b89561dbd77
Muhammed ElFeqy <thefeqy@gmail.com>
2025-02-14 21:39:35 +0200

    Chore: Implement user dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Thefeqy\ModelStatus\Middleware\EnsureAuthenticatedUserIsActive;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -14,11 +16,9 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth', EnsureAuthenticatedUserIsActive::class)->group(function () {
+    Route::get('/dashboard', DashboardController::class)->middleware(['verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
