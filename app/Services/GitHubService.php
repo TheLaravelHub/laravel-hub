d595017c07f815eda36254bf2f867924f74bdf7a
Muhammed ElFeqy <thefeqy@gmail.com>
2025-02-22 16:51:04 +0200

    feat: Add GitHub service to fetch repositories data by URL

---
+++ app/Services/GitHubService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+/**
+ * Class GitHubService
+ * @package App\Services
+ */
+class GitHubService
+{
+    /**
+     * Fetch repository data from GitHub API
+     *
+     * @param string $repoUrl
+     * @return array|null
+     */
+    public static function fetchRepositoryData(string $repoUrl): ?array
+    {
+        // Extract "owner/repo" from the URL
+        preg_match('/github\.com\/([^\/]+)\/([^\/]+)/', $repoUrl, $matches);
+        if (count($matches) < 3) {
+            return null;
+        }
+        [$fullMatch, $owner, $repo] = $matches;
+
+        $apiUrl = "https://api.github.com/repos/{$owner}/{$repo}";
+
+        $response = Http::withHeaders([
+            'Accept' => 'application/vnd.github.v3+json',
+            'Authorization' => 'Bearer ' . env('GITHUB_TOKEN'),
+        ])->get($apiUrl);
+
+        if ($response->failed()) {
+            return null;
+        }
+
+        $repoData = $response->json();
+
+        return [
+            'name' => $repoData['name'],
+            'description' => $repoData['description'],
+            'repository_url' => $repoData['html_url'],
+            'language' => $repoData['language'],
+            'stars' => $repoData['stargazers_count'],
+            'owner' => $repoData['owner']['login'],
+            'owner_avatar' => $repoData['owner']['avatar_url'],
+        ];
+    }
+}
