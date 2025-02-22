70d225b4cd1fce7934fe07cdaa27587aad9985a5
Muhammed ElFeqy <thefeqy@gmail.com>
2025-02-22 16:48:26 +0200

    chore: Add slug support trait

--- app/Models/Index.php
+++ app/Models/Index.php
@@ -2,16 +2,17 @@
 
 namespace App\Models;
 
+use App\Traits\HasSlug;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
-use Thefeqy\ModelStatus\Status;
 use Thefeqy\ModelStatus\Traits\HasActiveScope;
 
 class Index extends Model implements HasMedia
 {
     use HasActiveScope;
+    use HasSlug;
     use InteractsWithMedia;
     use SoftDeletes;
 
@@ -22,7 +23,7 @@ class Index extends Model implements HasMedia
     protected function casts()
     {
         return [
-            //            'status' => Status::class,
+            // 'status' => Status::class,
         ];
     }
 }
