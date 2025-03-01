6724907f403a08be4e2a79583b4fda3a6a7cac4f
Muhammed ElFeqy <thefeqy@gmail.com>
2025-03-01 15:30:06 +0200

    feat: add packages stats in the dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Package;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function __invoke()
     {
-        return Inertia::render('Admin/Dashboard/Index');
+        $packagesCount = Package::count();
+        $activePackagesCount = Package::query()->active()->count();
+        $inActivePackagesCount = Package::query()->inActive()->count();
+
+        return Inertia::render('Admin/Dashboard/Index',
+            compact('packagesCount', 'activePackagesCount', 'inActivePackagesCount'));
     }
 }
--- resources/js/Pages/Admin/Dashboard/Index.tsx
+++ resources/js/Pages/Admin/Dashboard/Index.tsx
@@ -1,7 +1,26 @@
 import AdminAuthenticatedLayout from '@/Layouts/AdminAuthenticatedLayout'
 import { Head } from '@inertiajs/react'
+import { LibraryBig } from 'lucide-react'
 
-export default function Dashboard() {
+interface DashboardProps {
+    packagesCount: number
+    activePackagesCount: number
+    inActivePackagesCount: number
+}
+
+export default function Dashboard({
+    packagesCount,
+    activePackagesCount,
+    inActivePackagesCount,
+}: DashboardProps) {
+    const activePercentage = (
+        (activePackagesCount / packagesCount) *
+        100
+    ).toFixed(2)
+    const inActivePercentage = (
+        (inActivePackagesCount / packagesCount) *
+        100
+    ).toFixed(2)
     return (
         <AdminAuthenticatedLayout breadcrumbs={[{ title: 'Dashboard' }]}>
             <Head title="Dashboard" />
@@ -11,114 +30,28 @@ export default function Dashboard() {
                 <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
                     <div className="rounded-xl border bg-card text-card-foreground shadow">
                         <div className="flex flex-row items-center justify-between space-y-0 p-6 pb-2">
-                            <div className="text-sm font-medium tracking-tight">
-                                Total Revenue
+                            <div className="text-md font-medium tracking-tight">
+                                Packages
                             </div>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                className="h-4 w-4 text-muted-foreground"
-                            >
-                                <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
-                            </svg>
+                            <LibraryBig />
                         </div>
                         <div className="p-6 pt-0">
-                            <div className="text-2xl font-bold">$45,231.89</div>
-                            <p className="text-xs text-muted-foreground">
-                                +20.1% from last month
+                            <div className="text-2xl font-bold">
+                                {packagesCount}
+                            </div>
+                            <p className="text-sm text-muted-foreground">
+                                Active packages:{' '}
+                                <span className="text-green-500">
+                                    {activePackagesCount}
+                                </span>{' '}
+                                ({`${activePercentage} %`})
                             </p>
-                        </div>
-                    </div>
-                    <div className="rounded-xl border bg-card text-card-foreground shadow">
-                        <div className="flex flex-row items-center justify-between space-y-0 p-6 pb-2">
-                            <div className="text-sm font-medium tracking-tight">
-                                Subscriptions
-                            </div>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                className="h-4 w-4 text-muted-foreground"
-                            >
-                                <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path>
-                                <circle
-                                    cx="9"
-                                    cy="7"
-                                    r="4"
-                                ></circle>
-                                <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75"></path>
-                            </svg>
-                        </div>
-                        <div className="p-6 pt-0">
-                            <div className="text-2xl font-bold">+2350</div>
-                            <p className="text-xs text-muted-foreground">
-                                +180.1% from last month
-                            </p>
-                        </div>
-                    </div>
-                    <div className="rounded-xl border bg-card text-card-foreground shadow">
-                        <div className="flex flex-row items-center justify-between space-y-0 p-6 pb-2">
-                            <div className="text-sm font-medium tracking-tight">
-                                Sales
-                            </div>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                className="h-4 w-4 text-muted-foreground"
-                            >
-                                <rect
-                                    width="20"
-                                    height="14"
-                                    x="2"
-                                    y="5"
-                                    rx="2"
-                                ></rect>
-                                <path d="M2 10h20"></path>
-                            </svg>
-                        </div>
-                        <div className="p-6 pt-0">
-                            <div className="text-2xl font-bold">+12,234</div>
-                            <p className="text-xs text-muted-foreground">
-                                +19% from last month
-                            </p>
-                        </div>
-                    </div>
-                    <div className="rounded-xl border bg-card text-card-foreground shadow">
-                        <div className="flex flex-row items-center justify-between space-y-0 p-6 pb-2">
-                            <div className="text-sm font-medium tracking-tight">
-                                Active Now
-                            </div>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                className="h-4 w-4 text-muted-foreground"
-                            >
-                                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                            </svg>
-                        </div>
-                        <div className="p-6 pt-0">
-                            <div className="text-2xl font-bold">+573</div>
-                            <p className="text-xs text-muted-foreground">
-                                +201 since last hour
+                            <p className="text-sm text-muted-foreground">
+                                Deactivated packages:{' '}
+                                <span className="text-red-500">
+                                    {inActivePackagesCount}
+                                </span>{' '}
+                                ({`${inActivePercentage} %`})
                             </p>
                         </div>
                     </div>
