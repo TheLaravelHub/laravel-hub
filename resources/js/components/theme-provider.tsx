97e22674a59552cfc4a93ab5facc06815084f6c7
Muhammed ElFeqy <thefeqy@gmail.com>
2025-03-02 04:38:10 +0200

    fix: Check if global window is defined

--- resources/js/components/theme-provider.tsx
+++ resources/js/components/theme-provider.tsx
@@ -26,9 +26,12 @@ export function ThemeProvider({
     storageKey = 'vite-ui-theme',
     ...props
 }: ThemeProviderProps) {
-    const [theme, setTheme] = useState<Theme>(
-        () => (localStorage.getItem(storageKey) as Theme) || defaultTheme,
-    )
+    const [theme, setTheme] = useState<Theme>(() => {
+        if (typeof window !== 'undefined') {
+            return (window.localStorage.getItem(storageKey) as Theme | null) || defaultTheme;
+        }
+        return defaultTheme;
+    });
 
     useEffect(() => {
         const root = window.document.documentElement
