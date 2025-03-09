3b9b74518ffee10005176088972a4f4561e4b960
Muhammed ElFeqy <thefeqy@gmail.com>
2025-03-09 06:07:30 +0200

    feat: add view action to index resource

---
+++ app/Filament/Resources/IndexResource/Pages/ViewIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Resources\IndexResource\Pages;
+
+use App\Filament\Resources\IndexResource;
+use Filament\Actions;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewIndex extends ViewRecord
+{
+    protected static string $resource = IndexResource::class;
+}
