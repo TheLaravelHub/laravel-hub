8ccd48ef146117695de05ebd296c862a72d96432
Muhammed ElFeqy <thefeqy@gmail.com>
2025-03-16 03:04:01 +0200

    feat: Schedule publishing blog posts

---
+++ app/Console/Commands/PublishScheduledPosts.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\BlogPost;
+use Illuminate\Console\Command;
+
+class PublishScheduledPosts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:publish-scheduled-posts';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publish scheduled blog posts when their scheduled time arrives';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $posts = BlogPost::needsPublishing()->get();
+
+        foreach ($posts as $post) {
+            $post->update([
+                'status' => 'published',
+                'published_at' => now(),
+            ]);
+
+            $this->info("Published: {$post->title}");
+        }
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -24,6 +25,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('app:publish-scheduled-posts')->everyMinute();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
