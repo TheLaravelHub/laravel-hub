76f3833f29a4576bcadd6b1909e07f7a2997a35b
Muhammed ElFeqy <thefeqy@gmail.com>
2025-03-16 05:15:35 +0200

    chore: SEO optimization

---
+++ resources/js/components/shared/AppHead.tsx
@@ -0,0 +1,10 @@
+import { Head } from '@inertiajs/react'
+
+export default function AppHead({ title, children }: { title: string, children: React.ReactNode }) {
+    return (
+        <Head>
+            <title>{title ? `${title} - My App` : 'My App'}</title>
+            {children}
+        </Head>
+    )
+}
