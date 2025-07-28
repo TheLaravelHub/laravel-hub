149dd49f9bc2d13500ae2d1b8ebebb2db3f555ce
Muhammed ElFeqy <thefeqy@gmail.com>
2025-07-28 20:01:14 +0300

    fix: Styling issues

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Actions\SendPasswordResetLinkAction;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
