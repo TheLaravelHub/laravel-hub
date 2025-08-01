5053c27701252d936a603f3803bfa4ed3e546cb4
Muhammed ElFeqy <thefeqy@gmail.com>
2025-08-01 19:20:54 +0300

    feat: Remove dark theme

--- resources/js/Pages/BlogPost.tsx
+++ resources/js/Pages/BlogPost.tsx
@@ -484,7 +484,7 @@ const BlogPost = ({ blogPost }: BlogPostProps) => {
                                                 </div>
                                             ) : (
                                                 <code
-                                                    className="rounded bg-gray-200 px-1 py-0.5 text-gray-800 dark:bg-gray-700 dark:text-gray-200"
+                                                    className="rounded bg-gray-200 px-1 py-0.5 text-gray-800"
                                                     {...props}
                                                 >
                                                     {children}
--- resources/js/components/theme-provider.tsx
+++ resources/js/components/theme-provider.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 
-type Theme = 'dark' | 'light' | 'system'
+type Theme = 'light'
 
 type ThemeProviderProps = {
     children: React.ReactNode
@@ -14,7 +14,7 @@ type ThemeProviderState = {
 }
 
 const initialState: ThemeProviderState = {
-    theme: 'system',
+    theme: 'light',
     setTheme: () => null,
 }
 
@@ -22,38 +22,19 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
 
 export function ThemeProvider({
     children,
-    defaultTheme = 'system',
+    defaultTheme = 'light',
     storageKey = 'vite-ui-theme',
     ...props
 }: ThemeProviderProps) {
-    const [theme, setTheme] = useState<Theme>(() => {
-        if (typeof window !== 'undefined') {
-            return (
-                (window.localStorage.getItem(storageKey) as Theme | null) ||
-                defaultTheme
-            )
-        }
-        return defaultTheme
-    })
+    const [theme, setTheme] = useState<Theme>('light')
 
     useEffect(() => {
         const root = window.document.documentElement
 
-        root.classList.remove('light', 'dark')
+        root.classList.remove('dark')
 
-        if (theme === 'system') {
-            const systemTheme = window.matchMedia(
-                '(prefers-color-scheme: dark)',
-            ).matches
-                ? 'dark'
-                : 'light'
-
-            root.classList.add(systemTheme)
-            return
-        }
-
-        root.classList.add(theme)
-    }, [theme])
+        root.classList.add('light')
+    }, [])
 
     const value = {
         theme,
