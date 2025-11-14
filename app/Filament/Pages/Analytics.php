729d51aecdb93ca6a38121a3501328ee3afd1f99
Muhammed ElFeqy <thefeqy@gmail.com>
2025-11-14 13:29:58 +0200

    feat(BlogPosts): Add blog posts analytics page

---
+++ app/Filament/Pages/Analytics.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Filament\Widgets\BlogViewsChart;
+use Filament\Pages\Page;
+
+class Analytics extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-chart-bar';
+
+    protected static string $view = 'filament.pages.analytics';
+
+    protected static ?string $navigationGroup = 'Blog';
+
+    protected static ?int $navigationSort = 3;
+
+    protected static ?string $title = 'Blog Analytics';
+
+    protected static ?string $navigationLabel = 'Analytics';
+
+    public function getWidgets(): array
+    {
+        return [
+            BlogViewsChart::class,
+        ];
+    }
+
+    public function getVisibleWidgets(): array
+    {
+        return $this->filterVisibleWidgets($this->getWidgets());
+    }
+
+    public function getHeaderWidgetsColumns(): int|array
+    {
+        return 1;
+    }
+}
